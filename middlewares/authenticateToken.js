0f413b760872a7ac15260fb04a4543a3a3e7ae0b
Samir-kumal <kumalsameer124@gmail.com>
2024-02-29 21:59:24 +0545

    chore: add console log message in case of error

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -5,7 +5,11 @@ const authenticateToken = (req, res, next) => {
 
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    
+    if (err){
+      console.log(err.message);
+      return res.sendStatus(403);
+    } 
     req.user = user;
     next();
   });
