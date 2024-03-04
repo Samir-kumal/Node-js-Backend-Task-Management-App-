4b3906fdd2be8896ba77f27292f006d2650161fc
Samir-kumal <kumalsameer124@gmail.com>
2024-03-04 14:55:42 +0545

    refactor: add route for getAllTaskItems function

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createTask, getTasks, updateTask, deleteTask } = require("../controllers/taskController");
+const { createTask, getTasks,getAllTasks,getAllTaskItems, updateTask, deleteTask } = require("../controllers/taskController");
 router.route("/tasks/:boardId").get(getTasks);
+router.route("/:userId/allTasks").get(getAllTasks);
+router.route("/:userId/allTaskItems").get(getAllTaskItems);
 router.route("/createTask/:boardId").post(createTask);
 router.route("/updateTask/:taskId").put(updateTask);
 router.route("/deleteTask/:taskId").delete(deleteTask);
