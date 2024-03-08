806197759c2dfa5bb8b58c12fe9e3ff63674c621
Samir-kumal <kumalsameer124@gmail.com>
2024-03-08 22:18:34 +0545

    modified

--- utils/sendVerificationMail.js
+++ utils/sendVerificationMail.js
@@ -1,22 +1,22 @@
-const createMailTransporter = require('./createMailTransporter');
+const createMailTransporter = require("./createMailTransporter");
 
 const sendVerificationMail = async (email, token) => {
   const transporter = createMailTransporter();
-  console.log("email verification Started....")
   const mailOptions = {
     from: `TaskFlow - Task Management App <${process.env.EMAIL}>`,
     to: email,
     secure: true,
     subject: "Account Verification",
-    html: `<p>Click <a href="http://localhost:5173/verify-email?emailToken=${token}"> click here</a> to verify your account</p>`,
+    html: `<p>Click <a href="https://taskflow-tms.vercel.app//verify-email?emailToken=${token}"> click here</a> to verify your account</p>`,
   };
+  console.log("email verification Started....");
+
   try {
     await transporter.sendMail(mailOptions);
-  console.log("email verification Completed....")
-
+    console.log("email verification Completed....");
   } catch (err) {
     console.log(err.message);
   }
-}
+};
 
 module.exports = sendVerificationMail;
