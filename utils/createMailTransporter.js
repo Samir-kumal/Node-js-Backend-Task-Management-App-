76d32f2d5425c070cf3bac8253d0444cce8e6a7e
Samir-kumal <kumalsameer124@gmail.com>
2024-10-17 19:35:00 +0545

    chore: minor updates

--- utils/createMailTransporter.js
+++ utils/createMailTransporter.js
@@ -1,15 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const createMailTransporter = () => {
-    const transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-    return transporter;
-    }
+  return transporter;
+};
 
-    module.exports = createMailTransporter;
+module.exports = createMailTransporter;
